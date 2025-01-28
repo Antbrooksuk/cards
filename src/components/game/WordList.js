1ed138c9610ce679cde585b7dc43036481ac7c80
Anthony <antbrooksuk@gmail.com>
2025-01-28 14:57:55 +0000

    rules

--- src/components/game/WordList.js
+++ src/components/game/WordList.js
@@ -13,31 +13,50 @@ const WordList = ({ allWords = [] }) => {
   return (
     <div className='played-words'>
       <h3 className='font-semibold' onClick={toggleExpand}>
-        {/* <svg
-          className={`${WORD_LIST_STYLES.CHEVRON} ${
-            isExpanded ? 'transform rotate-180' : ''
-          }`}
-          viewBox='0 0 20 20'
-          fill='currentColor'
-        >
-          <path
-            fillRule='evenodd'
-            d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-            clipRule='evenodd'
-          />
-        </svg> */}
-        Played Words ({allWords.length})
+        {allWords.length === 0
+          ? `Get started`
+          : `Played Words ${allWords.length}`}
       </h3>
-      <ul>
-        {[...allWords].reverse().map((wordObj, index) => (
-          <PlayedWord
-            key={index}
-            word={wordObj.word}
-            type={wordObj.type}
-            isInvalid={!wordObj.type}
-          />
-        ))}
-      </ul>
+      {allWords.length === 0 ? (
+        <ul>
+          <li>
+            <div>
+              <span>Try to make a word from the letters in your hand.</span>
+            </div>
+            <div>
+              <span className='breakdown'>
+                You can discard up to 9 letters per round.
+              </span>
+            </div>
+            <div>
+              <span className='breakdown'>
+                You have 5 plays to reach the round target.
+              </span>
+            </div>
+            <div>
+              <span className='breakdown'>
+                You can randomise your hand with the shuffle button.
+              </span>
+            </div>
+            <div>
+              <span className='breakdown'>
+                If you don't make the target score its game over!
+              </span>
+            </div>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          {[...allWords].reverse().map((wordObj, index) => (
+            <PlayedWord
+              key={index}
+              word={wordObj.word}
+              type={wordObj.type}
+              isInvalid={!wordObj.type}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
