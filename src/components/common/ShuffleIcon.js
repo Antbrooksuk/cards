7b9ad7cdabdad2bce7831094e3a4606e230d3321
Anthony <antbrooksuk@gmail.com>
2025-01-28 09:47:37 +0000

    fixes

--- src/components/common/ShuffleIcon.js
+++ src/components/common/ShuffleIcon.js
@@ -9,7 +9,7 @@ const ShuffleIcon = ({ className = '' }) => (
     strokeWidth='2'
     strokeLinecap='round'
     strokeLinejoin='round'
-    className={`w-6 h-6 ${className}`}
+    className={`w-8 h-8 ${className}`}
   >
     <polyline points='16 3 21 3 21 8' />
     <line x1='4' y1='20' x2='21' y2='3' />
